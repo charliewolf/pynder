3c170b0162ee308c521f3dd147b4b7ddf036e8dd
Charlie Wolf <charlie@wolf.is>
2015-08-20 16:29:24 -0500

    bump to 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-version = '0.0.4'
+version = '0.0.5'
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as file:
     long_description = file.read()
