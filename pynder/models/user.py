8db065580f651446eeee29b067090914e173c444
Casey Rodarmor <casey@rodarmor.com>
2015-09-04 00:43:39 -0400

    Add delete() method to Match model

--- pynder/models/user.py
+++ pynder/models/user.py
@@ -65,5 +65,8 @@ class Match(object):
     def message(self, body):
         return self._session._api.message(self.id, body)['_id']
 
+    def delete(self):
+        return self._session._api._request('DELETE', '/user/matches/' + self.id)
+
     def __repr__(self):
         return "<Unnamed match>" if self.user is None else repr(self.user)
