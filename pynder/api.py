e4f3a4c0fcbf0d4a288193c9559f7f0e76250a2a
Cthulhu <cthulhu@gbitnet.com.br>
2017-07-11 12:59:32 -0300

    full url refactor

--- pynder/api.py
+++ pynder/api.py
@@ -14,29 +14,34 @@ class TinderAPI(object):
         if XAuthToken is not None:
             self._session.headers.update({"X-Auth-Token": str(XAuthToken)})
 
-    def _url(self, base, path):
-        return base + path
+    def _full_url(self, url):
+        _url = url.lower()
+
+        if _url.startswith("http://") or _url.startswith("https://"):
+            return url
+        else:
+            return constants.API_BASE + url
 
     def auth(self, facebook_id, facebook_token):
         data = {"facebook_id": str(facebook_id), "facebook_token": facebook_token}
         result = self._session.post(
-            self._url(constants.API_BASE, '/auth'), json=data, proxies=self._proxies).json()
+            self._full_url('/auth'), json=data, proxies=self._proxies).json()
         if 'token' not in result:
             raise errors.RequestError("Couldn't authenticate")
         self._token = result['token']
         self._session.headers.update({"X-Auth-Token": str(result['token'])})
         return result
 
-    def _request(self, method, url, data={}, base=constants.API_BASE):
+    def _request(self, method, url, data={}):
         if not hasattr(self, '_token'):
             raise errors.InitializationError
-        result = self._session.request(method, self._url(
-            base, url), json=data, proxies=self._proxies)
+        result = self._session.request(method, self._full_url(
+            url), json=data, proxies=self._proxies)
         while result.status_code == 429:
             blocker = threading.Event()
             blocker.wait(0.01)
-            result = self._session.request(method, self._url(
-                base, url), data=data, proxies=self._proxies)
+            result = self._session.request(method, self._full_url(
+                url), data=data, proxies=self._proxies)
         if result.status_code < 200 or result.status_code >= 300:
             raise errors.RequestError(result.status_code)
         if result.status_code == 201 or result.status_code == 204:
@@ -65,12 +70,12 @@ class TinderAPI(object):
                             "xoffset_percent": float(x_offset), "yoffset_percent": float(y_offset)}]
                }
 
-        return self._request("post", "/media", data=data, base=constants.CONTENT_BASE)
+        return self._request("post", constants.CONTENT_BASE + "/media", data=data)
 
     def delete_profile_photo(self, photo_id):
         data = {"assets": [photo_id]}
 
-        return self._request("delete", "/media", data=data, base=constants.CONTENT_BASE)
+        return self._request("delete", constants.CONTENT_BASE + "/media", data=data)
 
     def recs(self, limit=10):
         return self._post("/user/recs", data={"limit": limit})
